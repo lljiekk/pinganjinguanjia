9358ab51e79b3c08bfc7e24dfea915c72e5bc9b4
jiekeke <jiekeke@163.com>
2019-12-27 00:10:33 +0800

    添加两个字段

--- compingan/src/main/java/User.java
+++ compingan/src/main/java/User.java
@@ -1,5 +1,8 @@
 public class User {
 
+    private String username="lianjie";
+    private String test="lianjieddd";
+
     public static void main(String[] args) {
         System.out.println("test");
     }
