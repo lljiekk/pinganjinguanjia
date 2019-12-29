f6da002d66760853a5f4e6e890540b08066b0735
jiekeke <jiekeke@163.com>
2019-12-29 17:14:01 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	jingguanjia/.idea/workspace.xml
    #	jingguanjia/src/main/java/UserTest.java

--- jingguanjia/src/main/java/UserTest.java
+++ jingguanjia/src/main/java/UserTest.java
@@ -2,7 +2,7 @@ public class UserTest {
 
     private String name2="ting";
     private String jjjdfdsfdsfdsfdname3="33333";
-    private String jjjdfdsfdsfdsfdname2="ting";
+    private String jjjdfdsfdsfdsfdname2="1111111111111";
 
     public static void main(String[] args) {
         System.out.println("luolianjie");
