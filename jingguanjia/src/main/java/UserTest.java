f7bb8dfd32db3c03f27cabf1d254deaa96518919
jiekeke <jiekeke@163.com>
2019-12-29 16:56:54 +0800

    clonecommit

--- jingguanjia/src/main/java/UserTest.java
+++ jingguanjia/src/main/java/UserTest.java
@@ -1,6 +1,7 @@
 public class UserTest {
 
     private String name2="ting";
+    private String jjjdfdsfdsfdsfdname3="33333";
 
     public static void main(String[] args) {
         System.out.println("luolianjie");
